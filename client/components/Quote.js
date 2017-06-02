cdba7301dc1e7434c80580325b329113c37ab44d
Tony Luisi <t.luisi@gmail.com>
2017-06-02 14:56:32 +1200

    removed proptypes

--- client/components/Quote.js
+++ client/components/Quote.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ErrorMessage from './ErrorMessage'
 import { fetchQuote, fetchSecretQuote } from '../actions'
