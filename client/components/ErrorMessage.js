691fe996b115b90f22c7db7c7857950ab595d88c
Tony Luisi <t.luisi@gmail.com>
2017-06-02 14:54:53 +1200

    convert to class extends component

--- client/components/ErrorMessage.js
+++ client/components/ErrorMessage.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const ErrorMessage = (props) => (
-    <div>
-       <p>{props.message}</p> 
-    </div>
-)
+class ErrorMessage extends React.Component {
+  render () {
+    return (
+      <div>
+        <p>{this.props.message}</p>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        message: state[ownProps.reducer].errorMessage
-    }
+  return {
+    message: state[ownProps.reducer].errorMessage
+  }
 }
 
 export default connect(mapStateToProps)(ErrorMessage)
